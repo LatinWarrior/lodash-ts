796e179b67445cc73310ecc4eadb065306684f40
Luis <luis.f.blanco@gmail.com>
2019-02-19 16:31:16 -0600

    save changes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import * as _ from 'lodash';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lodash-ts';
+
+
+  readonly friends = [
+    { id: 1, name: "Homer" },
+    { id: 2, name: "Bart" },
+    { id: 3, name: "Mr. Burns" }
+  ];
+
+  friend;
+
+  getFriend(id: number) {
+    this.friend = _.find(this.friends, ["id", id]);;
+  }
+
 }
